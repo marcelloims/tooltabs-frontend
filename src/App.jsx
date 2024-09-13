5d141259d8a0bb05fc4d79fa9a28e5dc33c2e311
Marcello Imanuel <marcelloimanuel25@gmail.com>
2024-09-14 01:20:30 +0800

    -Make feature login -Make a validation login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
-import Login from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/auth/Login";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <>
-            <Login />
-        </>
+        <div>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </div>
     );
 }
 
